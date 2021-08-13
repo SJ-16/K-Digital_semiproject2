aaaac05d31596d9cca0e4bbc1f71322bfedcd6d5
SJ-16 <qwer567103@gmail.com>
2021-08-13 09:42:57 +0900

    initial commit

--- myanalysis/data_preprocessing.py
+++ myanalysis/data_preprocessing.py
@@ -10,12 +10,7 @@ df2 = df.dropna(axis=0, subset=['age','height']); # na값 제거
 df2['age'] = df2['age'].astype('int64'); # int형으로 변환
 # df2['height'] = df2['height'].astype('int64');
 print(df2.info());
-# # # ----------------- xxl 제거시 ---------
-# cond = df2[df['size']=='XXL'].index;
-# df3 = df2.drop(cond);
-# print(df3.info()); # 119153 119084
-# # # ------------------------------------
 # print(df2);
+# df2.to_csv("Clothingsizedata.csv", index=False)
 # df2.to_csv("Clothingsizedata2.csv", index=False); # 반올림 안한 데이터
-# df3.to_csv("testdata.csv", index=False);
 
