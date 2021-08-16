de7b478e06460565ec4e46e489d884c8e56ecd1e
SJ-16 <qwer567103@gmail.com>
2021-08-16 18:20:56 +0900

    initial commit

--- myanalysis/kNN.py
+++ myanalysis/kNN.py
@@ -2,7 +2,20 @@
 
 ### 기본 라이브러리 불러오기
 import pandas as pd
-from config.settings import DATA_DIRS
+import seaborn as sns
+from config.settings import DATA_DIRS, STATICFILES_DIRS
+#----------------------plot 라이브러리 ----------------------------------
+import matplotlib
+import seaborn as sb
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import font_manager, rc
+from matplotlib.colors import ListedColormap
+font_path = "C:/Windows/Fonts/gulim.ttc"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font) ## 그래프를 알아보기위해 폰트 설정
+matplotlib.rcParams['axes.unicode_minus'] = False # 음수(-)값도 깨짐 방지
+#-----------------------------------------------------------------------
 
 '''
 [Step 1] 데이터 준비
@@ -25,6 +38,9 @@ print('\n')
 '''
 [Step 3] 분석에 사용할 속성을 선택
 '''
+# # Label Encoding
+# df['size'] = df['size'].map({'XXS': 1, 'S': 2, 'M' : 3,
+#                              'L' : 4, 'XL' : 5, 'XXL' : 6, 'XXXL' : 7})
 
 # 분석에 활용할 열(속성)을 선택
 ndf = df[['weight', 'age', 'height', 'size']]
@@ -34,14 +50,19 @@ print('\n')
 # # 원핫인코딩 - 범주형 데이터를 모형이 인식할 수 있도록 숫자형으로 변환
 # onehot_size = pd.get_dummies(ndf['size'])
 # ndf = pd.concat([ndf, onehot_size], axis=1)
-#
-#
-#
 # ndf.drop(['size'], axis=1, inplace=True)
 # print(ndf.head())
 # print('\n')
 # print(ndf)
 
+
+# # Data Heatmap
+# ndf_corr = ndf.corr()
+# sb.heatmap(ndf_corr, cmap = 'Blues', annot = True, xticklabels = ndf_corr.columns.values, yticklabels = ndf_corr.columns.values)
+# plt.title('Data Heatmap', fontsize = 15)
+# plt.xticks(fontsize = 12)
+# plt.yticks(fontsize = 12)
+# plt.savefig(STATICFILES_DIRS[0]+'//heatmap.png')
 '''
 [Step 4] 데이터셋 구분 - 훈련용(train data)/ 검증용(test data)
 '''
@@ -51,7 +72,37 @@ X=ndf[['weight', 'age', 'height']]  #독립 변수 X
 y=ndf['size']       #종속 변수 Y
 print('--------------------------------------------22222222222222222222222');
 print(X);
+#--------------------------------------------
+# scatter visualization
+# sb.scatterplot('weight', 'age', data = ndf, hue = 'size', palette = 'Set2', edgecolor = 'b', s = 150,
+#                alpha = 0.7)
+# plt.title('weight / age')
+# plt.xlabel('weight')
+# plt.ylabel('age')
+# plt.legend(loc = 'upper left', fontsize = 12)
+# plt.savefig(STATICFILES_DIRS[0]+'//weight_age.png')
+# import seaborn as sb
+# sb.scatterplot('weight', 'height', data = ndf, hue = 'size', palette = 'Set2', edgecolor = 'b', s = 150,
+#                alpha = 0.7)
+# plt.title('weight / height')
+# plt.xlabel('weight')
+# plt.ylabel('height')
+# plt.legend(loc = 'upper left', fontsize = 12)
+# plt.savefig(STATICFILES_DIRS[0]+'//weight_height.png')
+# import seaborn as sb
+# sb.scatterplot('height', 'age', data = ndf, hue = 'size', palette = 'Set2', edgecolor = 'b', s = 150,
+#                alpha = 0.7)
+# plt.title('height / age')
+# plt.xlabel('height')
+# plt.ylabel('age')
+# plt.legend(loc = 'upper left', fontsize = 12)
+# plt.savefig(STATICFILES_DIRS[0]+'//height_age.png')
 
+# # Scatter Matrix
+# sb.pairplot(data = ndf, hue = 'size',palette = 'magma')
+# plt.savefig(STATICFILES_DIRS[0]+'//pairplot.png')
+# plt.show();
+#----------------------------------------
 # 설명 변수 데이터를 정규화(normalization)
 from sklearn import preprocessing
 X = preprocessing.StandardScaler().fit(X).transform(X)
@@ -106,16 +157,53 @@ from sklearn.metrics import accuracy_score
 knn_acc = accuracy_score(y_test,y_hat);
 print(knn_acc);
 
+
+
+# # scatter plot
 #
+# n_neighbors = 228
 #
-# X=ndf[['pclass', 'age', 'sibsp', 'parch', 'female', 'male',
-#        'town_C', 'town_Q', 'town_S']]
-# data = pd.DataFrame({'pclass':[1],'age':[26],'sibsp':[0],'parch':[0],'female':[0],'male':[1],'town_C':[1],'town_Q':[0],'town_S':[0] })
-# #data = pd.DataFrame({'pclass':[3],'age':[32],'sibsp':[0],'parch':[0],'female':[0],'male':[1],'town_C':[0],'town_Q':[1],'town_S':[0] })
+# X = X[:, :2]
+#
+# y= y.target
+# h = .02  # step size in the mesh
+#
+# cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+# cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+#
+# for weights in ['uniform', 'distance']:
+#     clf = KNeighborsClassifier(n_neighbors, weights=weights)
+#     clf.fit(X, y)
+#
+#     # Plot the decision boundary. For that, we will assign a color to each
+#     # point in the mesh [x_min, x_max]x[y_min, y_max].
+#     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+#     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+#     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+#                          np.arange(y_min, y_max, h))
+#     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+#
+#     # Put the result into a color plot
+#     Z = Z.reshape(xx.shape)
+#     plt.figure()
+#     plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+#
+#     # Plot also the training points
+#     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+#                 edgecolor='k', s=20)
+#     plt.xlim(xx.min(), xx.max())
+#     plt.ylim(yy.min(), yy.max())
+#     plt.title("3-Class classification (k = %i, weights = '%s')"
+#               % (n_neighbors, weights))
+#
+# plt.show()
+
+# X=ndf[['weight', 'age', 'height']]
+# data = pd.DataFrame({'weight':[59],'age':[36],'height':[167.6]})
+# #data = pd.DataFrame({'weight':[3],'age':[32],'height':[0],'size':[0]})
 # data1 = pd.concat([X,data]);
 # print(data1);
 # X = preprocessing.StandardScaler().fit(data1).transform(data1)
 # y_hat2 = knn.predict(X)
 # print(y_hat2)
 # print(y_hat2[len(y_hat2)-1])
-
